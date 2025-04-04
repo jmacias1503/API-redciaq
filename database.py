e46ae888132bb68af78ef56fc21e739ae894207d
Alejandro Macias <alex.mac.fon@gmail.com>
2025-04-04 13:38:05 -0600

    fix: made string mongodb uri nad db

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB = os.getenv("MONGO_DB")
+MONGO_URI = str(os.getenv("MONGO_URI"))
+MONGO_DB = str(os.getenv("MONGO_DB"))
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client[MONGO_DB]
